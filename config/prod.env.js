75da2b7ae0b8a0f1729e2f9fcf38bd05c7d5b95d
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-06 18:32:17 +0400

    Changing URL

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://laravelvueapps.herokuapp.com/api"'
+  API_URL: '"https://projectwithlaravelvuejs.herokuapp.com/api"'
 }
