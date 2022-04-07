1669af4bb828607115224a6d1959822b50a5cbc0
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-07 10:48:02 +0400

    URL

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   //API_URL: '"http://127.0.0.1:8000/api"'
-  API_URL: '"https://projectwithlaravelvuejs.herokuapp.com"'
+  API_URL: '"https://projectwithlaravelvuejs.herokuapp.com/api"'
 })
