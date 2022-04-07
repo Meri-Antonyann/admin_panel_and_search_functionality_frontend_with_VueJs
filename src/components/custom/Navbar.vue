70ef25c136276d63bfd5f73a07e6df4c12834e73
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-07 15:12:21 +0400

    Changing navbar

--- src/components/custom/Navbar.vue
+++ src/components/custom/Navbar.vue
@@ -1,35 +1,81 @@
 <template>
+
   <div>
-    <b-navbar type="dark" variant="dark">
-      <b-navbar-nav >
-        <b-nav-item>
-          <router-link to="/">
-            Home
-          </router-link>
-        </b-nav-item>
-        <b-nav-item>
-          <router-link to="/login">
-            Login
-          </router-link>
-        </b-nav-item>
-        <b-nav-item>
-          <router-link to="/register">
-            Registration
-          </router-link>
-        </b-nav-item>
-        <b-nav-item>
-          <router-link to="/posts">
-            Posts
-          </router-link>
-        </b-nav-item>
-        <b-nav-item v-if="token">
-          <router-link to="/logout">
-            logout
-          </router-link>
-        </b-nav-item>
-      </b-navbar-nav>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item  >
+            <router-link to="/login">Login</router-link>
+            </b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
+
+
+
+
+
+<!--  <div>-->
+<!--    <b-navbar type="dark" variant="dark">-->
+<!--      <b-navbar-nav >-->
+<!--        <b-nav-item>-->
+<!--          <router-link to="/">-->
+<!--            Home-->
+<!--          </router-link>-->
+<!--        </b-nav-item>-->
+<!--        <b-nav-item>-->
+<!--          <router-link to="/login">-->
+<!--            Login-->
+<!--          </router-link>-->
+<!--        </b-nav-item>-->
+<!--        <b-nav-item>-->
+<!--          <router-link to="/register">-->
+<!--            Registration-->
+<!--          </router-link>-->
+<!--        </b-nav-item>-->
+<!--        <b-nav-item>-->
+<!--          <router-link to="/posts">-->
+<!--            Posts-->
+<!--          </router-link>-->
+<!--        </b-nav-item>-->
+<!--        <b-nav-item v-if="token">-->
+<!--          <router-link to="/logout">-->
+<!--            logout-->
+<!--          </router-link>-->
+<!--        </b-nav-item>-->
+<!--      </b-navbar-nav>-->
+<!--    </b-navbar>-->
+<!--  </div>-->
 </template>
 
 <script>
