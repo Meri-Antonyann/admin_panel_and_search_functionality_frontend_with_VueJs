f5348bf84f6656cfd606ac97e2c0cdfa66c77602
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-07 15:21:42 +0400

    Changing NavBar

--- src/components/custom/Navbar.vue
+++ src/components/custom/Navbar.vue
@@ -18,32 +18,19 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
+
 
           <b-nav-item-dropdown text="Lang" right>
             <b-dropdown-item  >
             <router-link to="/login">Login</router-link>
             </b-dropdown-item>
-            <b-dropdown-item  >
-              <router-link to="/register">Login</router-link>
-            </b-dropdown-item>
+
             <b-dropdown-item  >
               <router-link to="/Log out">Login</router-link>
             </b-dropdown-item>
 
           </b-nav-item-dropdown>
 
-<!--          <b-nav-item-dropdown right>-->
-<!--            &lt;!&ndash; Using 'button-content' slot &ndash;&gt;-->
-<!--            <template #button-content>-->
-<!--              <em>User</em>-->
-<!--            </template>-->
-<!--            <b-dropdown-item href="#">Profile</b-dropdown-item>-->
-<!--            <b-dropdown-item href="#">Sign Out</b-dropdown-item>-->
-<!--          </b-nav-item-dropdown>-->
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
