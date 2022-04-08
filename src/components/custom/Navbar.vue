843956873988a4bb54dac9bdfc15db9b236c4984
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-08 17:26:21 +0400

    Commit

--- src/components/custom/Navbar.vue
+++ src/components/custom/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info" class="fixed-top" style="max-width:inherit;margin:auto">
+    <b-navbar toggleable="lg" type="dark" variant="info" class="fixed-top" style="max-width:inherit; ">
 
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
