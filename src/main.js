ba31ebf193a583932417a6023e327decf8749ead
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-09 17:37:03 +0400

    just

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from "./store";
-import pagination from "laravel-vue-pagination"
+import pagination from 'laravel-vue-pagination'
 
 require('./plugins')
 
