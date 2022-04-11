107264682ad0b612c820b1e8d0af10e7100e0bf4
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-11 11:35:08 +0400

    just

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // API_URL: '"https://projectwithlaravelvuejs.herokuapp.com/api"'
-  API_URL: '"http://127.0.0.1:8000/api"'
+  API_URL: '"https://projectwithlaravelvuejs.herokuapp.com/api"'
+  // API_URL: '"http://127.0.0.1:8000/api"'
 }
