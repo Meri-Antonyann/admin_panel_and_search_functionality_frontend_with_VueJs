06eaf42ec932cf2bc7f8647884425e630d2d02ef
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-12 15:30:33 +0400

    just

--- src/components/user/AdminPostComponent.vue
+++ src/components/user/AdminPostComponent.vue
@@ -81,7 +81,12 @@
 
   methods:{
     async getPosts(){
-      await this.axios.get('post').then(response=>{
+      await this.axios.get('post?page=' + this.pagination.current_page,{
+        params: {
+          'page': this.pagination.currentPage,
+            'per_page': this.pagination.perPage,
+        },
+      }).then(response=>{
         console.log(response.data.posts)
         this.posts = response.data.posts.data
         this.pagination  = response.data.posts ;
