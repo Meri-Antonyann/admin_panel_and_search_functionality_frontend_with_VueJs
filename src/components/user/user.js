5b31b67f5f6de13793c9b269aacc7288bb807ec3
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-04-12 12:23:07 +0400

    just

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -2,6 +2,7 @@ import Profile from "./ProfileComponent";
 import Posts from "./AdminPostComponent";
 import Edit from "./EditPostComponent";
 
+const page = this.pagination.current_page
 export default [
   { path: '/profile', name: 'Profile', component: Profile },
   { path: `/posts/${page}`, name: 'Posts', component: Posts },
